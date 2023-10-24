e17e027e4aec0623badf0861c904ad419b01e7c9
Bruno Silva <s.bruno.12@hotmail.com>
2023-10-24 05:59:20 -0300

    first commit

---
+++ src/exercicio/CalculaMedia.java
@@ -0,0 +1,16 @@
+package exercicio;
+
+public class CalculaMedia {
+    public static void main(String[] args) {
+    	
+        double nota1 = 7.5;
+        double nota2 = 8.0;
+        double nota3 = 9.2;
+        double nota4 = 6.8;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
